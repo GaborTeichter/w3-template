8707863b68a0c25523caa0fb2a94953879c0c4fc
GaborTeichter <66086127+GaborTeichter@users.noreply.github.com>
2021-06-20 19:37:18 +0200

    I finished the style of the subpages and fixed the top bar color

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -34,4 +34,14 @@ function myAccFunc() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
   }
+
+  // Accordions
+  function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
   
